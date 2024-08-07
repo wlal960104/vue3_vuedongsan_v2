67fe9f3535a295aa22b32d0e20ed3e074ee849f2
jimi <jimi96@softworks.co.kr>
2024-08-07 11:44:02 +0900

    [24.08.07] List.vue 생성

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,9 @@
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="#">Home</a>
           </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Products</a>
-          </li>
+          <!--<li class="nav-item">-->
+          <!--  <a class="nav-link" href="#">Products</a>-->
+          <!--</li>-->
           <li class="nav-item">
             <a class="nav-link" href="#">Join</a>
           </li>
@@ -25,13 +25,22 @@
       </div>
     </div>
   </nav>
+
+  <!-- 부동산 리스트 -->
+  <List/>
+
+
+
+
 </template>
 
 <script>
+import List from "@/components/List.vue";
 
 export default {
   name: 'App',
   components: {
+    List
   }
 }
 </script>
@@ -43,5 +52,69 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.navbar {
+  margin-bottom: 10px;
+}
+
+.bd-placeholder-img {
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
+
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
+
+.b-example-divider {
+  width: 100%;
+  height: 3rem;
+  background-color: rgba(0, 0, 0, 0);
+  border: solid rgba(0, 0, 0, .15);
+  border-width: 1px 0;
+  box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+}
+
+.b-example-vr {
+  flex-shrink: 0;
+  width: 1.5rem;
+  height: 100vh;
+}
+
+.bi {
+  vertical-align: -.125em;
+  fill: currentColor;
+}
+
+.nav-scroller {
+  position: relative;
+  z-index: 2;
+  height: 2.75rem;
+  overflow-y: hidden;
+}
+
+.nav-scroller .nav {
+  display: flex;
+  flex-wrap: nowrap;
+  padding-bottom: 1rem;
+  margin-top: -1px;
+  overflow-x: auto;
+  text-align: center;
+  white-space: nowrap;
+  -webkit-overflow-scrolling: touch;
+}
+
+.bd-mode-toggle {
+  z-index: 1500;
+}
+
+.bd-mode-toggle .dropdown-menu .active .bi {
+  display: block !important;
 }
 </style>
---
+++ src/components/List.vue
@@ -0,0 +1,97 @@
+<template>
+  <main>
+    <div class="album py-5">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img card-img-top" focusable="false" height="225"
+                   preserveAspectRatio="xMidYMid slice" role="img" width="100%"
+                   xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title>
+                <rect fill="#55595c" height="100%" width="100%"/>
+                <text dy=".3em" fill="#eceeef" x="50%" y="50%">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">타이틀</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                  <small class="text-body-secondary">2024.08.06 날짜</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img card-img-top" focusable="false" height="225"
+                   preserveAspectRatio="xMidYMid slice" role="img" width="100%"
+                   xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title>
+                <rect fill="#55595c" height="100%" width="100%"/>
+                <text dy=".3em" fill="#eceeef" x="50%" y="50%">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">타이틀</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                  <small class="text-body-secondary">2024.08.06 날짜</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img card-img-top" focusable="false" height="225"
+                   preserveAspectRatio="xMidYMid slice" role="img" width="100%"
+                   xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title>
+                <rect fill="#55595c" height="100%" width="100%"/>
+                <text dy=".3em" fill="#eceeef" x="50%" y="50%">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">타이틀</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                  <small class="text-body-secondary">2024.08.06 날짜</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col">
+            <div class="card shadow-sm">
+              <svg aria-label="Placeholder: Thumbnail" class="bd-placeholder-img card-img-top" focusable="false" height="225"
+                   preserveAspectRatio="xMidYMid slice" role="img" width="100%"
+                   xmlns="http://www.w3.org/2000/svg"><title>Placeholder</title>
+                <rect fill="#55595c" height="100%" width="100%"/>
+                <text dy=".3em" fill="#eceeef" x="50%" y="50%">Thumbnail</text>
+              </svg>
+              <div class="card-body">
+                <p class="card-text">타이틀</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                  </div>
+                  <small class="text-body-secondary">2024.08.06 날짜</small>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'List',
+}
+</script>
+
+<style>
+
+</style>
